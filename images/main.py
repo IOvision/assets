7e7d7493863d78c47b760995073807e99dc50947
Danish Jamal <danishjamal.104@gmail.com>
2020-11-26 22:45:50 +0530

    file renamed to lower

---
+++ images/main.py
@@ -0,0 +1,18 @@
+import os
+
+
+def isDir(name):
+    return len(name.split('.')) == 1
+
+
+dirs = os.listdir()
+c = 0
+for i in dirs:
+    if isDir(i):
+        nd = os.listdir(i)
+        for j in nd:
+            pth = "{}/{}".format(i,j)
+            sp = pth.split('.')
+            sp[-1] = sp[-1].lower()
+            npth = '.'.join(sp)
+            os.rename(pth, npth)
